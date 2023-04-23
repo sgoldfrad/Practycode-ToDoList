ee4efde49f72bab8d21e2136de4c44522ea0c43f
sgoldfrad <sgold8822@gmail.com>
2023-04-23 18:16:12 +0300

    fix

--- Experience1-ToDoList/ToDoApi/Program.cs
+++ Experience1-ToDoList/ToDoApi/Program.cs
@@ -23,15 +23,15 @@ var app = builder.Build();
 
 app.UseCors("EnablePolicy");
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
    app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
-}
+//}
 
 app.UseSwagger(options =>
 {
